c5b77549b32a2201aee5240f622d888f24f46976
stem league <64894084+stemleague@users.noreply.github.com>
2021-06-28 14:34:58 -0700

    Create space_travel.py

---
+++ space_travel.py
@@ -0,0 +1,106 @@
+## PART 1: Debugging ############################################
+# Find the bug in this piece of code. Note that with the height 72, your code should print "mesosphere" based off the Space Travel slides on slide 7. 
+##########################################################
+height = 72
+
+if height > 20:
+  print('stratosphere')
+elif height > 50:
+  print('mesosphere')
+elif height > 100:
+  print('space')
+else:
+  print('troposhere') 
+  
+  
+#### PART 2: Temperature Testing ####
+#### YOUR CODE HERE ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### PART 3: Practice with conditionals #####
+############################################
+# Now that we are off planet 🌍 and into space, we flew off into the dark abyss of space. Not much longer, we landed on a planet at the same time the space ship's location identifier broke. However, when we checked our distance traveled from Earth, our distance meter reads 155 million miles.
+# We have to fix this issue to identify the planet we have landed on. 
+# First, set a variable distance equal to 155 since that is our distance meter's current reading
+# To reprogram your space ship to figure out the planet you landed on based off the distance meter, write an if/elif/else conditional that meets the requirement of the following:
+# If distance meter reads between 90-404 million miles (inclusive), then it landed on planet Mars (print Mars)
+# If distance meter reads 405-859 million miles (inclusive), then it landed on planet Jupiter (print Jupiter)
+# If distance meter reads 860-1900 million miles (inclusive), then it landed on Saturn (print Saturn)
+# If distance meter reads 1901-2787 million miles (inclusive), then it landed on Neptune (print Neptune)
+##########################################################
+#### YOUR CODE HERE ####
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+## PART 4: Conditionals within conditionals
+##############################################
+# Now that you've been able to identify which planet you have landed on, you need to reconfigure your system based off the planet you've arrived in to find a place to settle your space ship for the night. You've set up a couple markers around your space ship and measured their distances from the the flag that your teammates found around 200 miles from your space ship. 
+# The commanders says that you must follow these code requirements to determine if your space ship location is safe:
+# If the markers read "green" then you must check:
+# 1) If the marker is within the 400 mile radius of the flag (inclusive), print "safe"
+# 2) If the marker is more than 400 mile radius of the flag, print "not safe"
+# If the markers read "yellow" then you must check:
+# 1) If the marker is within the 300 mile radius of the flag (inclusive), print "safe"
+# 2) If the marker is more than 300 mile radius of the flag, print "not safe"
+# If the markers read "red" then you must check:
+# 1) If the marker is within the 200 mile radius of the flag (inclusive), print "safe"
+# 2) If the marker is more than 200 mile radius of the flag, print "not safe"
+# First start with setting a string variable marker_color to "red" & another integer variable marker_distance to 145
+# NOTE: You are comparing strings and also comparing integers and you will have conditionals within conditionals in your code
+########################################################## 
+#### YOUR CODE HERE ####
+
+
+
+  
+  
+  
+  
+  
+  
+  
