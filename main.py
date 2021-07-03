d3a55caece359bff21fda8ad0568498348b8582d
stem league <64894084+stemleague@users.noreply.github.com>
2021-07-03 10:55:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -24,23 +24,14 @@ else:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
 #### PART 3: Practice with conditionals #####
 ############################################
 # Now that we are off planet 🌍 and into space, we flew off into the dark abyss of space. Not much longer, we landed on a planet at the same time the space ship's location identifier broke. However, when we checked our distance traveled from Earth, our distance meter reads 155 million miles.
 # We have to fix this issue to identify the planet we have landed on. 
-# First, set a variable distance equal to 155 since that is our distance meter's current reading
-# To reprogram your space ship to figure out the planet you landed on based off the distance meter, write an if/elif/else conditional that meets the requirement of the following:
+
+
+# First, set a variable `distance` equal to 155 since that is our distance meter's current reading. To reprogram your space ship to figure out the planet you landed on based off the distance meter, write an if/elif/else conditional that meets the requirement of the following:
+
 # If distance meter reads between 90-404 million miles (inclusive), then it landed on planet Mars (print Mars)
 # If distance meter reads 405-859 million miles (inclusive), then it landed on planet Jupiter (print Jupiter)
 # If distance meter reads 860-1900 million miles (inclusive), then it landed on Saturn (print Saturn)
@@ -56,27 +47,6 @@ else:
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
 ## PART 4: Conditionals within conditionals
 ##############################################
 # Now that you've been able to identify which planet you have landed on, you need to reconfigure your system based off the planet you've arrived in to find a place to settle your space ship for the night. You've set up a couple markers around your space ship and measured their distances from the the flag that your teammates found around 200 miles from your space ship. 
@@ -97,10 +67,8 @@ else:
 
 
 
-  
-  
-  
-  
-  
-  
+
+
+
+
   
